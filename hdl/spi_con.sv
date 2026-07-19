3ccf69b06e8115e005bf6daa92df6f4168efc441
Zacck Osiemo <coderv63@gmail.com>
2026-07-19 10:55:52 +0200

    Implement spi controller module
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

--- hdl/spi_con.sv
+++ hdl/spi_con.sv
@@ -20,6 +20,8 @@ module spi_con
 
 	logic [31:0]bit_counter;
 	logic [31:0]clk_period_count;
+	logic [DATA_WIDTH-1: 0] copi_buffer;
+	logic [DATA_WIDTH-1: 0] cipo_buffer;
 
 	always_ff @(posedge clk) begin 
 		if(rst) begin 
@@ -31,10 +33,12 @@ module spi_con
 		end
 	end
 
+	// run dclk at 50% duty
 	always_ff @(posedge clk) begin
 		if(trigger) begin 
 			cs <= 0;
 			bit_counter <= DATA_WIDTH - 1;
+			copi_buffer <= data_in;	
 		end else begin
 			if(cs == 0) begin
 				if (clk_period_count == 0) begin
@@ -49,6 +53,30 @@ module spi_con
 				end
 			end
 		end 
+	end
+
+	//set copi to MSB of copi buffer 
+	//shift copi_buffer by 1 to the left
+	// reduce bit counter 
+	// when done with bits 
+	// raise cs 
+	// when dclk goes high 
+	// read cipo to cipo_buffer
+	always_ff @(posedge clk) begin 
+		if(cs == 0) begin
+			if (bit_counter == 0) begin 
+				cs <= 1;
+				data_valid <= 1; 
+				data_out <= cipo_buffer;
+			end else if((dclk == 0) && (clk_period_count == DATA_CLK_PERIOD - 1)) begin 
+				copi <= copi_buffer[DATA_WIDTH-1];
+				copi_buffer <= {copi_buffer[DATA_WIDTH-2:0], 1'b0};
+				bit_counter <= bit_counter - 1;
+			end else if((dclk == 1) && (clk_period_count == ((DATA_CLK_PERIOD >> 1) - 1))) begin 
+				cipo_buffer <= {cipo_buffer[DATA_WIDTH-2:0], cipo};
+			end
+		end else 
+			data_valid <= 0;
 	end
 endmodule
 
